2d6c67cfbcb77b3fe15f2b26cfd7f09123ce0ff2
Tbxxx <tl_burnett@comcast.net>
2015-04-12 18:37:52 -0700

    Update with user configurable support file - support.cfg
    support.cfg support directories configuration - exit.com in \cpm_com, a
    few other changes.

--- bin/ccp_spt.c
+++ bin/ccp_spt.c
@@ -7,6 +7,7 @@
 
 /* drive assignments for support files */
 #include "drives.h"
+#define MAXLINE 128
 
 extern char altfiles[6][256];
 
@@ -161,9 +162,18 @@ int spacepad(char *str, int len)
  * It always loads at least two directory names into altfiles[0] and [1] 
  * They are to be sequentially searched for any open request.
  * The drive location is appended at the time of fopen()
+ *
+ * Changed 4/12/2015 to use support.cfg so users can change/add support
+ * directories for programs/packages without needing a recompile or
+ * C programming expertise.
  */
 void set_altfiles(char *name)
 {
+	FILE *fps;
+	char inbuf[MAXLINE];
+	char *progptr;
+	char *dirptr;
+	int len;
 
 	/* Auto path add depending on filename
 	 * If the program is not known, generic and other are used.
@@ -180,72 +190,55 @@ void set_altfiles(char *name)
 	strcpy(redir_drive_prefix[0], "./");
 	strcpy(altfiles[1], "./"); /* drive A: same as default */
 	strcpy(redir_drive_prefix[1], "./");
+
+	/* [2] (the specific support directory) filled in below */
 	
-	strcpy(altfiles[3], CPMCOM);  /* drive B: */
+	strcpy(altfiles[3], CPMCOM);
 	strcpy(redir_drive_prefix[2], CPMCOM);
 	
 	strcpy(altfiles[4], CPMCOM); 
 	strcpy(redir_drive_prefix[4], CPMCOM);
 
-
-	/* note altfiles[2] (drive C: )set per individual program(s) below
-	 */
-		/* MICROSOFT  - *.rel files, bcload, brun.com, etc.
-		 * microsoft basic and any variation of m80 assembler (e.g.m801,
-		 * m80new), bascom basic compiler and l80 linker.  Also Werner's
-		 * enhanced m80 and f80 fortran compiler, obasic 4.51 and ld80 disk
-		 * linker
-		 */	
-	if ((strncmp(name, "mbasi", 5)==0) || (strncmp(name, "m80", 3)== 0) ||
-		(strcmp(name, "bascom")==0) || (strcmp(name, "l80")== 0) ||
-		(strcmp(name, "f80")== 0) || (strncmp(name, "obasi", 5)==0) ||
-		(strcmp(name, "ld80")== 0)) {
-		strcpy(altfiles[2], MS);
-		strcpy(redir_drive_prefix[2], MS);
-		return;
-	} else if (strncmp(name, "bds", 3) == 0) {
-		/* BDS C */
-		/* begins with bds - support = *.crl, *.h, cc2.com, etc. */
-		/* use directory */
-		strcpy(altfiles[2], BDS);
-		strcpy(redir_drive_prefix[2], BDS);
-		return;
-	} else if (strncmp(name, "az", 2)==0) {
-		/* Aztec C 
-		 * anything that begins with az - *.h, *.rel, *.lib, etc.
-		 */
-		/* use directory */
-		strcpy(altfiles[2], AZTEC);
-		strcpy(redir_drive_prefix[2], AZTEC);
-		return;
-	} else if (strncmp(name, "ht", 2)==0) {
-		/* Hitech C */
-		/* anything that beings with ht - *.h, whatever */
-		/* use directory */
-		strcpy(altfiles[2], HTC);
-		strcpy(redir_drive_prefix[2], HTC);
-		return;
-	} else if ((strcmp(name, "mac")==0) || (strcmp(name, "rmac")==0) ||
-		(strcmp(name, "drilink")==0) || (strcmp(name, "drilib")==0)) {
-		/* DRI drilink,  drilib - *.lib macros e.g. z80.lib */
-		/* use directory */
-		strcpy(altfiles[2], DRI);
-		strcpy(redir_drive_prefix[2], DRI);
-		return;
-	} else if (strncmp(name, "slr", 3)==0) {
-		/* SLR Systems */
-		/* anything that begins with slr - includes, macros, etc. */
-		/* use directory */
-		strcpy(altfiles[2], SLR);
-		strcpy(redir_drive_prefix[2], SLR);
-		return;
-	} else {
-		/* no special directory - when filenames conflict another support
-		 * directory should be added in the above logic.
-		 */
-		strcpy(altfiles[2], GENERIC);
-		strcpy(redir_drive_prefix[2], GENERIC);
+	if ((fps = fopen("support.cfg", "r")) == NULL) {
+		fprintf(stderr, "Please create a support.cfg file\n");
+		fprintf(stderr, "It may be empty\n");
+		exit(1);
 	}
+	while ((fgets(inbuf, MAXLINE-1, fps)) != NULL) {
+		if (strlen(inbuf) < 10) {
+			/* skip invalid (or trailing blank!) lines */
+			continue;
+		}
+		if (inbuf[0] == '#') {
+			/* skip comment line */
+			continue;
+		}
+		/* strip newline */
+		inbuf[strlen(inbuf)-1] = '\0';
+
+		/* get both fields */
+		progptr = strtok(inbuf, ",\n");
+		dirptr = strtok(NULL, ",\n");
+		/* find matching program name or prefix and store */
+		if (strchr(inbuf, '*')) {	/* use strncmp for wildcard */
+			if (strncmp(name, progptr, strlen(progptr)-1) == 0) {
+				/* found it - load with target directory */
+				strcpy(altfiles[2], dirptr);
+				strcpy(redir_drive_prefix[2], dirptr);
+				/* done! */
+				break;
+			}
+		} else {	/* no wildcard */
+			if (strcmp(name, progptr) == 0) {
+				/* found it - load with target directory */
+				strcpy(altfiles[2], dirptr);
+				strcpy(redir_drive_prefix[2], dirptr);
+				/* done! */
+				break;
+			}
+		}
+	}
+	fclose(fps);
 	return;
 		
 }
